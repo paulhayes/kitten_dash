bb0562c90bb967b412f97aad8d8d74bcb58f47cd
Paul Hayes <paul.hayes@entropedia.co.uk>
2015-08-18 18:45:04 +0100

    Added crossplatform input and added respawn script

---
+++ Assets/Scripts/SaveSpot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveSpot : MonoBehaviour {
+
+	static Vector3 position = Vector3.zero;
+
+	void Awake(){
+		if( position != Vector3.zero ){
+			transform.position = position;			
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){ 
+		if(collider.gameObject.tag == "Respawn"){
+			Debug.Log("Respawn point "+collider.transform.position);
+			position = collider.transform.position;			
+		}
+	}
+}
