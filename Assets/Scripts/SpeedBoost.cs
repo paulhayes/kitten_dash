5b4dfd1d78dde8df21bec565384c233c4bc1af5f
Paul Hayes <paul.hayes@entropedia.co.uk>
2015-08-19 22:41:18 +0100

    Added speed boost and speed boost particle effect. Needs art work

---
+++ Assets/Scripts/SpeedBoost.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedBoost : MonoBehaviour {
+
+	public float boostSpeed;
+	public float duration;
+	
+	bool boosting;
+
+	IEnumerator OnCollisionEnter2D(Collision2D collision){
+	
+		if( collision.gameObject.tag == "SpeedBoost" ){
+			Debug.Log(collision.gameObject.tag);
+			if( boosting ) yield break;
+			boosting = true;
+			GetComponentInChildren<ParticleSystem>().Play();
+			RunAndJump runScript = GetComponent<RunAndJump>();
+			float originalSpeed = runScript.runningSpeed;
+			runScript.runningSpeed = boostSpeed;
+			yield return new WaitForSeconds(duration);
+			runScript.runningSpeed = originalSpeed;
+			GetComponentInChildren<ParticleSystem>().Stop();
+			boosting = false;
+		}
+	}
+
+}
