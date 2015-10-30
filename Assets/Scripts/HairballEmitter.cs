22279c71aad41710f5dc4cfc2f577892eaf336eb
Blooker <evangriffiths@hotmail.co.uk>
2015-10-30 14:05:37 +0000

    Pushing added code

---
+++ Assets/Scripts/HairballEmitter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HairballEmitter : MonoBehaviour {
+
+	public GameObject hairballPrefab;
+	public Transform spawnPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject hairball = Instantiate (hairballPrefab);
+		hairball.transform.position = spawnPosition.position;
+
+	}
+}
